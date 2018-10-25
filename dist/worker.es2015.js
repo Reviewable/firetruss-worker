dd221b6fefd3f5c975bb50c46bd4890c709d0ba5
Piotr Kaminski <piotr@ideanest.com>
2018-10-25 14:00:54 -0700

    release 1.1.2

--- dist/worker.es2015.js
+++ dist/worker.es2015.js
@@ -3,7 +3,7 @@
 var fireworkers = [];
 var apps = {};
 // This version is filled in by the build, don't reformat the line.
-var VERSION = 'dev';
+var VERSION = '1.1.2';
 
 
 var LocalStorage = function LocalStorage() {
